ba69e890fb2dfad6a86f0eced7206e154816f11e
Vlad <neurohunt@gmail.com>
2022-09-20 17:15:01 +0300

    New

--- js/custom.js
+++ js/custom.js
@@ -169,3 +169,10 @@ $(document).ready(function () {
 
     });
 });
+
+var map = L.map('map', {
+    crs: L.CRS.Simple
+});
+var bounds = [[0,0], [1000,1000]];
+var image = L.imageOverlay('img/LV.jpg', bounds).addTo(map);
+map.fitBounds(bounds);
